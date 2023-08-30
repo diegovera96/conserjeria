d07eb6b3b02e605cd388a2a7d6b635eebc0551e7
Diego_Vera <diego.vera@alumnos.ucn.cl>
2023-08-30 19:46:52 -0400

    Initial commit

---
+++ conserjeria/src/main/java/cl/ucn/disc/as/Main.java
@@ -0,0 +1,57 @@
+package cl.ucn.disc.as;
+
+
+import cl.ucn.disc.as.dao.PersonaFinder;
+import cl.ucn.disc.as.model.Persona;
+import io.ebean.DB;
+import io.ebean.Database;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.DatabaseMetaData;
+import java.util.Optional;
+
+/**
+ *The Main
+ *@author Profesor Diego Urrutia-Astorga
+  */
+@Slf4j
+public class Main {
+    /**
+    *The Main
+     *
+     * @param args to use.
+     */
+    public static void main(String[] args){
+
+        log.debug("Starting Main..");
+
+        log.debug("Starting Main..");
+
+        //get the database
+        Database db = DB.getDefault();
+
+        Persona persona = Persona.builder()
+                .rut(999999)
+                .nombre("Diego")
+                .apellidos("Vera Santis")
+                .email("diego@gmail.com")
+                .telefono("123456789")
+                .build();
+
+        log.debug("The Persona before db: ${}", persona);
+
+        //save into the database
+        db.save(persona);
+
+        log.debug("The Persona after db: ${}", persona);
+
+        //finder de personas
+        PersonaFinder pf = new PersonaFinder();
+        Optional<Persona> oPersona = pf.byRut(999999);
+        oPersona.ifPresent(p -> log.debug("Persona from db: {}", p));
+
+
+        log.debug("Done. :)");
+
+    }
+}
