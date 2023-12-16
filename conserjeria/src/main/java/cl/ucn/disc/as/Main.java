bfa13d02513d7ffc522eabdf96438f2821703378
Diego_Vera <diego.vera@alumnos.ucn.cl>
2023-12-16 04:05:09 -0300

    fix: grpc

--- conserjeria/src/main/java/cl/ucn/disc/as/Main.java
+++ conserjeria/src/main/java/cl/ucn/disc/as/Main.java
@@ -2,6 +2,7 @@ package cl.ucn.disc.as;
 
 
 import cl.ucn.disc.as.dao.PersonaFinder;
+import cl.ucn.disc.as.grpc.PersonaGrpcServiceImpl;
 import cl.ucn.disc.as.model.Contrato;
 import cl.ucn.disc.as.model.Edificio;
 import cl.ucn.disc.as.model.Persona;
@@ -12,10 +13,12 @@ import io.ebean.Database;
 
 import cl.ucn.disc.as.ui.ApiRestServer;
 import cl.ucn.disc.as.ui.WebController;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import lombok.extern.slf4j.Slf4j;
 import io.javalin.Javalin;
 
-
+import java.io.IOException;
 import java.sql.DatabaseMetaData;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -27,18 +30,28 @@ import java.util.Optional;
  *@author Profesor Diego Urrutia-Astorga
   */
 @Slf4j
-public class Main {
+public final class TheMain {
     /**
     *The Main
      *
      * @param args to use.
      */
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException, IOException {
 
-        log.debug("Starting Main..");
+        log.debug("Starting Main  with library path: {}.", System.getProperty("java.library.path"));
         Javalin app = ApiRestServer.start(7070, new WebController());
 
-        app.stop();
+        //app.stop();
+        log.debug("Starting the gRPC server..");
+        Server server = ServerBuilder
+                .forPort(50123)
+                .addService(new PersonaGrpcServiceImpl())
+                .build();
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+
+        server.awaitTermination();
 
         log.debug("Done. :)");
 
